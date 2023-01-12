67f61945c5a0a782e21ac9e87e801a0de0a245d7
Prathaphr37 <prathaphr33@gmail.com>
2023-01-12 12:57:50 +0530

    message

---
+++ src/test/java/simple/P2.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		System.out.println("i am prathap");
+		System.out.println("from mysore");
+		System.out.println("gmit college");
+		System.out.println("mandya");
+		
+	}
+}
