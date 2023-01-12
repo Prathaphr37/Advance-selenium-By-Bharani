a7e58eae48d25e8dcfea77092233f46a7f13d9ef
Prathaphr37 <prathaphr33@gmail.com>
2023-01-12 12:34:39 +0530

    second commit

---
+++ src/test/java/simple/P1.java
@@ -0,0 +1,10 @@
+package simple;
+
+public class P1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("How are you");
+	}
+}
